708455a37d4e6679b2f825a860623c17537ff5e1
Ben Samuels-Kalow <bensamuelskalow@gmail.com>
2017-05-08 11:01:48 -0400

    yup

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ function setup() {
 function draw() {
     background("white");
     imageMode("center");
-	// makes the picture the height of the window on tall screens and the width on narrow ones
+    // makes the picture the height of the window on tall screens and the width on narrow ones
     if (windowWidth > windowHeight) {
         image(img, windowWidth / 2, windowHeight / 2, windowHeight, windowHeight);
     } else {
@@ -26,23 +26,23 @@ function draw() {
 
     // then rotate the line around the pivot point by a number of degrees equal to the frame count of the sketch
 
-// three steady hands...
-	stroke("grey");
+    // three steady hands...
+    stroke("grey");
     rotate(radians(frameCount / 2));
     strokeWeight(10);
     line(0, 0, 50, 100);
 
-    rotate(radians(frameCount));
+    rotate(radians(-1 * frameCount));
     strokeWeight(5);
     line(0, 0, 10, 75);
 
-    rotate(radians(-1 * frameCount / 5));
+    rotate(radians( frameCount / 5));
     strokeWeight(20);
     line(0, 0, 10, 50);
 
-// and one erratic one...
+    // and one erratic one...
     stroke("gold");
-    rotate(radians(frameCount * random(-1, 1)));
+    rotate((frameCount * random(-1, 1)));
     strokeWeight(3);
     line(0, 0, 10, 100);
 
